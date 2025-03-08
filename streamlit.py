ee45411a1b4d3f5bc7d1619f859430a0a88b7657
aritra43 <mukhopadhyayaritra7@gmail.com>
2025-03-08 17:27:01 +0530

    comnew

--- streamlit.py
+++ streamlit.py
@@ -11,10 +11,6 @@ from dotenv import load_dotenv
 from crewai_tools import FileReadTool, FileWriterTool
 import streamlit as st
 
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-
 load_dotenv()
 
 # Title
